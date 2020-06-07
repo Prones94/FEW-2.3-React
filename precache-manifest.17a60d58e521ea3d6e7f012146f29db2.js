05afac9f92c98b988c790b9f7368f5e2f4b4a2dc
Ian Rones <ronesp.85@gmail.com>
2020-06-06 21:23:19 -0700

    Updates

---
+++ precache-manifest.17a60d58e521ea3d6e7f012146f29db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866c3d985d04333a5089a4a00527b41e",
+    "url": "/FEW-2.3-React/index.html"
+  },
+  {
+    "revision": "afd9b18903df160d5b54",
+    "url": "/FEW-2.3-React/static/css/main.61a6ae03.chunk.css"
+  },
+  {
+    "revision": "c7e45c81e334e282e804",
+    "url": "/FEW-2.3-React/static/js/2.128bd5dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FEW-2.3-React/static/js/2.128bd5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd9b18903df160d5b54",
+    "url": "/FEW-2.3-React/static/js/main.6535622e.chunk.js"
+  },
+  {
+    "revision": "6a430d63cfed5e82355f",
+    "url": "/FEW-2.3-React/static/js/runtime-main.5019e2e4.js"
+  }
+]);
