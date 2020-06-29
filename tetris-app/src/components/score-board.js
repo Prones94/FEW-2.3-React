bf9dee57c239bafb4e3dedf78a7cdc7751a2aa84
Ian Rones <ronesp.85@gmail.com>
2020-06-29 16:43:11 -0700

    [add] grid area styles

--- tetris-app/src/components/score-board.js
+++ tetris-app/src/components/score-board.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import '../styles/ScoreBoard.css'
 
 class ScoreBoard extends Component {
+    constructor(props){
+        super(props);
+        this.handlePlay = this.handlePlay.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
     handlePlay(evt){
 
     }
@@ -13,7 +19,7 @@ class ScoreBoard extends Component {
             <div className="score-board">
                 <div>Score: { this.props.score }</div>
                 <div>Level: 1</div>
-                <button className="score-board-button" onClick = {this.handlePlay}></button>
+                <button className="score-board-button" onClick = {this.handlePlay}>Play</button>
                 <button className="score-board-button" onClick = {this.handleReset}>Restart</button>
             </div>
         )
