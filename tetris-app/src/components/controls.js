ffbf04568cc1be5a7a7dba1e8d750725e6daf105
Ian Rones <ronesp.85@gmail.com>
2020-06-30 12:16:54 -0700

    [add] connections for controls

--- tetris-app/src/components/controls.js
+++ tetris-app/src/components/controls.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 import '../styles/Controls.css'
+import { connect } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
+const mapStateToProps = state => {
+    return {
+        isRunning: state.game.isRunning
+    }
+}
+
+const mapDispatchToProps = () => {
+    return {
+        moveRight,
+        moveLeft, 
+        moveDown,
+        rotate
+    }
+}
 
 class Controls extends Component {
     constructor(props){
@@ -9,20 +25,27 @@ class Controls extends Component {
         this.handleRight = this.handleRight.bind(this);
         this.handleRotate = this.handleRotate.bind(this);
         this.handleDown = this.handleDown.bind(this);
+        this.isRunning = this.props
     }
+    
     handleLeft(evt){
-
+        if (!this.isRunning) { return }
+        this.props.moveLeft()
     }
     handleRight(evt){
-
+        if (!this.isRunning) { return }
+        this.props.moveRight()
     }
     handleRotate(evt){
-
+        if (!this.isRunning) { return }
+        this.props.rotate()
     }
     handleDown(evt){
-
+        if (!this.isRunning) { return }
+        this.props.moveDown()
     }
     render(){
+
         return (
             <div className="controls">
                 <button className="control-button" onClick={this.handleLeft}>Left</button>
@@ -34,4 +57,4 @@ class Controls extends Component {
     }
 }
 
-export default Controls
+export default connect( mapStateToProps, mapDispatchToProps())(Controls)
