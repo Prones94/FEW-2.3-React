e83dfc75f08f56225365a05b0f446118308a580d
Ian Rones <ronesp.85@gmail.com>
2020-06-29 20:45:11 -0700

    [add] state utils

--- tetris-app/src/utils/index.js
+++ tetris-app/src/utils/index.js
@@ -1,3 +1,146 @@
 export const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+export const gridDefault = () => {
+    const rows = 18
+    const cols = 10
+    const array = []
+    for (let row = 0; row < rows; row++) {
+        array.push([])
+        for (let col = 0; col < cols; cols++){
+            array[row].push(0)
+        }
+    }
+    return array
+}
+
+export const randomShape = () => {
+    return random(1, shapes.length -1)
+}
+
+export const defaultState = () => {
+    return {
+        grid: gridDefault(), // creates an empty grid
+        shape: randomShape(), // creates a new random shape
+        rotation: 0,  // set rotation of the shape is 0
+        x: 5, // sets the 'x' position of the shape to 5 and y to -4, which puts the 
+              // shape in the center of the grid, above the top
+        y: -4, 
+        nextShape: randomShape(), // sets the index of the next shape to a new random shape
+        isRunning: true, // Tell the game that it's currently running
+        score: 0,// Set the score to 0
+        speed: 1000,// Set the default speed
+        gameOver: false  // Game isn't over yet
+    }
+}
+ 
+export const shapes = [
+    // none
+    [[[0,0,0,0],
+      [0,0,0,0],
+      [0,0,0,0],
+      [0,0,0,0]]],
+  
+    // I
+    [[[0,0,0,0],
+      [1,1,1,1],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0]]],
+  
+    // T
+    [[[0,0,0,0],
+      [1,1,1,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [1,1,0,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [1,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,1,0],
+      [0,1,0,0],
+      [0,0,0,0]]],
+  
+    // L
+    [[[0,0,0,0],
+      [1,1,1,0],
+      [1,0,0,0],
+      [0,0,0,0]],
+  
+     [[1,1,0,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,0,1,0],
+      [1,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,0,0],
+      [0,1,1,0],
+      [0,0,0,0]]],
+  
+    // J
+    [[[1,0,0,0],
+      [1,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,1,0],
+      [0,1,0,0],
+      [0,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,0,0,0],
+      [1,1,1,0],
+      [0,0,1,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,0,0],
+      [1,1,0,0],
+      [0,0,0,0]]],
+  
+    // Z
+    [[[0,0,0,0],
+      [1,1,0,0],
+      [0,1,1,0],
+      [0,0,0,0]],
+  
+     [[0,0,1,0],
+      [0,1,1,0],
+      [0,1,0,0],
+      [0,0,0,0]]],
+  
+    // S
+    [[[0,0,0,0],
+      [0,1,1,0],
+      [1,1,0,0],
+      [0,0,0,0]],
+  
+     [[0,1,0,0],
+      [0,1,1,0],
+      [0,0,1,0],
+      [0,0,0,0]]],
+  
+    // O
+    [[[0,1,1,0],
+      [0,1,1,0],
+      [0,0,0,0],
+      [0,0,0,0]]]
+  ]
