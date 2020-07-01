d275f899e22fe3b3454159231c7f56f994d946cc
Ian Rones <ronesp.85@gmail.com>
2020-06-30 21:18:50 -0700

    [add] connection for message popup

--- tetris-app/src/components/message-popups.js
+++ tetris-app/src/components/message-popups.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
 import '../styles/Message.css'
+import { connect } from 'react-redux'
+
+
+const mapStateToProps = state => {
+    return {
+        gameOver: state.game.gameOver,
+        isRunning: state.game.isRunning
+    }
+}
+
+const mapDispatchToProps = () => {
+    return { }
+}
 
 
 class MessagePopup extends Component{
     render(){
+        const { gameOver, isRunning } = this.props
+        let message = ''
+        let isHidden = 'hidden'
+
+        // If game is over, show "Game Over"
+        if (gameOver) {
+            message = "Game Over"
+            isHidden = ''
+        // If game isn't running and paused, show "Paused"
+        } else if (!isRunning) {
+            message = "Paused"
+            isHidden = ''
+        // Default message, which is hidden
+        } else {
+            message = ""
+        }
         return(
-            <div className="message-popup">
-                <h1>Message Title</h1>
-                <p>Message info...</p>
+            <div className={`message-pop-up ${isHidden}`}>
+                <h1>{message}</h1>
+                <p></p>
             </div>
         )
     }
 }
 
-export default MessagePopup
+export default connect(mapStateToProps, mapDispatchToProps())(MessagePopup)
